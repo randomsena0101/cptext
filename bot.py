036474d6c69bbf5501b3bc96e8e3da0aa48f7de7
randomsena0101 <166689915+randomsena0101@users.noreply.github.com>
2024-04-13 09:45:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,9 +43,9 @@ logging.basicConfig(
 
 bot = Client(
     "bot",
-    api_id= 22779671,
-    api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
+    api_id= 27862677,
+    api_hash= "e343ce2c81b2b6c2c0d6bee58284e3bd",    
+    bot_token= "6991599062:AAFwn6ubznUzL51Wi5jH0EaODVyck3w4atQ"
 )
 
 @bot.on_message(filters.command(["cp"]))
